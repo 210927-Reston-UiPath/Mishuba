b57cec59473ba2270fa4797e9a8bd290ea5e8830
Mishuba <mishuba14@gmail.com>
2021-09-30 23:42:23 -0400

    Tycadome

--- Tycadome/TestingTycadomeApp/Program.vb
+++ Tycadome/TestingTycadomeApp/Program.vb
@@ -6,6 +6,11 @@ Module Program
         dim Priceless as Items = "items.json"
         dim ItemName as String = ""
         
+    Console.WriteLine("Let's begin here shall we")
+    
+    dim HolyGrail as boolean = True 
+    do while HolyGrail
+
 
         Console.WriteLine("Hello World!")
     End Sub
