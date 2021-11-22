2fe07234874f9569dda7ea6a0ed38b9355238213
Mishuba <mishuba14@gmail.com>
2021-11-22 10:35:29 -0500

    Note edits

--- Website/script.js
+++ Website/script.js
@@ -1,27 +1,3 @@
-// the shopping cart javascript below
-function buy(name) {
-    let cart = document.querySelector('#cart tbody');
-
-    let newItem = cart.insertRow();
-    let itemName = newItem.insertCell(0);
-    itemName.innerHTML = name;
-
-    let elementID = '#' + name.replaceAll(' ', '');
-
-    let itemPrice = newItem.insertCell(1);
-    itemPrice.innerHTML = document.querySelector(elementID + ' .price').innerHTML;
-
-    let itemQuantity = newItem.insertCell(2);
-    itemQuantity.innerHTML = document.querySelector(elementID + ' td .quantity').value;
-
-    document.querySelector(elementID + ' td .quantity').value = '';
-}
-
-function checkout() 
-{
-    document.querySelectorAll('#cart tbody tr').forEach(row => row.remove());
-}
-// end of the shopping cart section
 
 //Working on the news sections
 function tick() {
@@ -31,6 +7,32 @@ function tick() {
     } 
     setInterval(function() { tick () }, 5000);
 // End of the news section ticker.
+
+// Community post update section
+
+// end of coummunity post update section
+
+// music player section
+
+// end of music player section
+
+// random facts section
+function tickRandomFacts() {
+    $('#TheList li:first').slideUp( function() {
+        $(this).appendTo($('#TheList')).slideDown();
+    });
+} 
+setInterval(function() { tickRandomFacts () }, 5000);
+// end random facts section
+
+//Business info (The footer one) ticker in the footer.
+function tickBusiness() {
+    $('#BizTickerScroll li:first').slideUp( function() {
+        $(this).appendTo($('#BizTickerScroll')).slideDown();
+    });
+} 
+setInterval(function() { tickBusiness () }, 10000);
+// End of business info (The footer one) section ticker.
 
 /* The game homepage game javascript code here */
 var MishubaHomeGame = document.getElementById("MishubaHomeGame");
@@ -59,3 +61,28 @@ var checkDead = setInterval(function(){
 },10);
 
 //End of the javascript HomePageGame
+
+// the shopping cart javascript below
+function buy(name) {
+    let cart = document.querySelector('#cart tbody');
+
+    let newItem = cart.insertRow();
+    let itemName = newItem.insertCell(0);
+    itemName.innerHTML = name;
+
+    let elementID = '#' + name.replaceAll(' ', '');
+
+    let itemPrice = newItem.insertCell(1);
+    itemPrice.innerHTML = document.querySelector(elementID + ' .price').innerHTML;
+
+    let itemQuantity = newItem.insertCell(2);
+    itemQuantity.innerHTML = document.querySelector(elementID + ' td .quantity').value;
+
+    document.querySelector(elementID + ' td .quantity').value = '';
+}
+
+function checkout() 
+{
+    document.querySelectorAll('#cart tbody tr').forEach(row => row.remove());
+}
+// end of the shopping cart section
